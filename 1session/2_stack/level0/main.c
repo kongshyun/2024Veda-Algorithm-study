39d3d68f631d611634e2df9f751810e0279d983d
kongshyun <tlgus927@naver.com>
2024-08-09 18:46:59 +0900

    [1 session] stack - level0

--- 1session/2_stack/level0/main.c
+++ 1session/2_stack/level0/main.c
@@ -1,10 +1,17 @@
-//1 Session - 스택(Stack) - level0
+
 /*
+1 Session - 스택(Stack) - level0
+
+[목적: 기능구현]
+- 기능만 동작하면 OK (시간복잡도 고려 X)
+------------------------------------------------------------------
+[스터디 메모]
 #define TRUE 뒤에 괄호 하고 1을 쓰는게 좋다! 나중에 안엇갈림.
 
-
-
+------------------------------------------------------------------
+0809 혼자서 전부 다 구현해보기
 */
+
 #include <stdio.h>
 #include <assert.h>
 
@@ -21,16 +28,19 @@ int is_empty(void);
 int is_full(void);
 int pop(void);
 
+/*  값을 끝에서부터 하나씩 넣어줌 / 가득 찬 상태이면 한칸씩 앞으로 밀고 제일 끝에 다시 넣어줌. */
 void push(int value)
 {
     if(is_full()==TRUE){
         printf("FULL.\n");
-        for(int i=0;i<MAX_NUMS-1;i++){
-            s_nums[i]=s_nums[i+1];
+        for(int i=MAX_NUMS-1;i>0;i--){
+            s_nums[i-1]=s_nums[i]; 
+            //한칸씩 앞으로 값을 넘긴다.  s[10]값이 s[9]로, s[9]값이 s[8]로,,,
+            //앞으로 한칸씩 밀어서 s_nums[MAX-1]자리를 비워준다.
         }
-        s_nums[MAX_NUMS -1] =value;
+        s_nums[MAX_NUMS -1] = value; // s[MAX] 비었으므로 value 값을 넣어준다.
     }else{
-        s_nums[s_num_count++]=value;
+        s_nums[s_num_count++] = value; //빈공간의 끝에서부터 value값을 push한다.
     }    
 }
 
@@ -41,7 +51,6 @@ int is_empty(void)
 
 int is_full(void)
 {
-    
     return s_num_count==MAX_NUMS;
 }
 
@@ -50,7 +59,7 @@ int pop(void){
         printf("EMPTY.\n");
         return -1;
     }
-    return s_nums[--s_num_count];
+    return s_nums[s_num_count--]; // 끝에서부터 값을 하나씩 꺼낸다.
 }
 
 
