734b74d996f9611e39d875ce273931b6f3d1f5b0
kongshyun <tlgus927@naver.com>
2024-08-12 03:01:28 +0900

    0811 linked_list

--- 3_linkedlist/level1/linked_list.c
+++ 3_linkedlist/level1/linked_list.c
@@ -1,4 +1,11 @@
 #include "linked_list.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+node_t* get_node_or_null(const node_t* head, int id){
+
+}
 
 int add(node_t** phead, node_t node){
 
--- 3_linkedlist/prepractice.c
+++ 3_linkedlist/prepractice.c
@@ -1,7 +1,9 @@
 
 /* 혀니 C 코딩 */
 
+
 #include <stdio.h>
+#include <stdlib.h>// maaloc,free
 #include <assert.h>
 
 #define TRUE (1)
@@ -20,22 +22,28 @@ typedef struct node{
     //다음 노드의 주소를 저장하는 포인터 여야 하므로, 노드형의 포인터 변수 선언
 }node_t;
 
-node_t* head=NULL;//아직 노드가 생성되지 않은 상태.
+node_t* head=NULL;//첫 노드의 주소를 저장하는 포인터(8byte)  
 node_t* tail=NULL;
 
 int s_nums[MAX_NUMS];
 int s_num_count =0;
 
 //삽입
-int push(int value)
+int push(int data)
 {
-    while(is_full()== TRUE)
-    {
-        s_nums[++s_num_count]=value;
-        head=s_nums[s_num_count];
-        head -> next;
-    }
+    node_t* newNode;
+    newNode=(node_t*)malloc(sizeof(node_t)); // 노드 생성!
+    newNode->value=data;
+    newNode ->next=NULL;
+    if( head==NULL){
+         head=newNode;
+         return;
+    }//head가 newNode를 가르킴
+    newNode ->next=head;
+    head=newNode;//newNode가 다시 head가됨.
 }
+
+
 
 //삭제
 int pop(){
@@ -45,18 +53,18 @@ int pop(){
     while(head!=NULL){
         head=head->next;   // next는 어떻게 head의 next인지 아나?
         free(delNode); // delNode값을 삭제한다.
-        delNode=head; //delNode를 다시 head로 위치
+        delNode=head; //delNode를 다 시 head로 위치
     }
     //while문 종료후 -> head == NULL, delNode == NULL
 }
 
 
 int is_pull(){
-    return ;
+    return s_num_count == MAX_NUMS;
 }
 
 int is_empty(){
-    return ;
+    return s_num_count==0;
 }
 
 
