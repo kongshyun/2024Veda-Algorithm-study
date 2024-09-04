6196c247e08285c5c6851c8d3784b6f8fdf3d885
kongshyun <tlgus927@naver.com>
2024-09-05 00:34:36 +0900

    [7_재귀함수]PRE과제, 코드 준비

---
+++ 7_recursive/LEVEL0-a/fac.c
@@ -0,0 +1,5 @@
+#include "fac.h"
+
+int factorial(int n){
+    
+}
---
+++ 7_recursive/LEVEL0-a/main.c
@@ -0,0 +1,10 @@
+#include<assert.h>
+#include "fac.h"
+
+int main(void)
+{
+    assert(factorial(5) == 120);
+    assert(factorial(4) == 120);
+
+	return 0;
+}
